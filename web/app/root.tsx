fce35965613056c70eb854f9f12226916fddf89f
Jayson J. Phillips <58239+jaysonjphillips@users.noreply.github.com>
2022-05-29 12:29:06 -0400

    PWA support

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -7,27 +7,28 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useLocation,
+  useMatches,
 } from "@remix-run/react";
+import { useEffect } from "react";
 import NavBar from "./components/NavBar";
-
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
-
-
 import styles from "./styles/tailwind.css";
-
 export const links: LinksFunction = () => {
   return [
     { rel: "stylesheet", href: styles },
-    {rel: "preconnect", href:"https://fonts.googleapis.com"},
-    {rel: "preconnect", href:"https://fonts.gstatic.com"},
-    {href: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500&display=swap", rel: "stylesheet"},
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com" },
+    {
+      href: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500&display=swap",
+      rel: "stylesheet",
+    },
   ];
 };
-
 export async function loader() {
   return json({
     ENV: {
@@ -36,22 +37,53 @@ export async function loader() {
     },
   });
 }
-
 export default function App() {
   const data = useLoaderData();
+  let location = useLocation();
+  let matches = useMatches();
+
+  let isMount = true;
+  useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: "REMIX_NAVIGATION",
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: "REMIX_NAVIGATION",
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener("controllerchange", listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener(
+            "controllerchange",
+            listener
+          );
+        };
+      }
+    }
+  }, [location]);
 
   return (
     <html lang="en">
       <head>
-        <Meta />
-        <Links />
+        <Meta /> <Links />
       </head>
       <body className="main-bg">
-        <NavBar />
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <NavBar /> <Outlet /> <ScrollRestoration /> <Scripts /> <LiveReload />
       </body>
     </html>
   );
